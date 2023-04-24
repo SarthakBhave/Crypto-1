45c9d82e7a23e226ed905a650bfb03d478f9f8e2
SarthakBhave <96856998+SarthakBhave@users.noreply.github.com>
2023-04-25 00:13:45 +0530

    Update start-monero.sh

--- start-monero.sh
+++ start-monero.sh
@@ -1,2 +1,2 @@
 cd ./xmrig/build/
-./xmrig -o gulf.moneroocean.stream:10128 -u 46ac9WxjD46KgAE49AqZdy9j4D7nERY9F1Xnsq1istsxUw1QTSKTS7VSEiYoT9yhtB7628wenTKUsMFAgZawoN8x7vRx242 -p --cuda Worker0
+./xmrig -o gulf.moneroocean.stream:10128 -u 46ac9WxjD46KgAE49AqZdy9j4D7nERY9F1Xnsq1istsxUw1QTSKTS7VSEiYoT9yhtB7628wenTKUsMFAgZawoN8x7vRx242 -p Worker0
